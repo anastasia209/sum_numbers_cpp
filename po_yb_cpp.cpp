388b1dfae9847c012162dd5ba9d43d704b59eaf9
Анастасия <boskoridzi@gmail.com>
2026-03-23 22:47:06 +0300

    Add files via upload

---
+++ po_yb_cpp.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <Windows.h>
+#include <iostream>
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+
+		int number, num;
+		int sum = 0;
+
+		std::cout << "Введите целое число: " << std::endl;
+		std::cin >> number;
+
+		while (number > 0) {
+			num = number % 10;
+			sum += num; 
+			number /= 10; 
+		}
+
+		std::cout << "Сумма цифр: " << sum << std::endl;
+
+		return 0;
+	}
+
+	
